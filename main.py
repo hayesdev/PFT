37f70380e0c6f4c907aa6b74844c4bb6973151d0
Greg Hayes <gthelight@gmail.com>
2024-12-02 13:42:33 -0700

    adds add_entry

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 
+import pandas as pd
+import csv
+from datetime import datetime
 
-def main():
-    print("Hello world!")
+
+class CSV:
+    CSV_FILE = "finance_data.csv"
+    COLUMNS = ["date", "amount", "category", "description"]
+
+    @classmethod
+    def inititalize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE, index=False)
+
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }
+        # using this context manager Python will automatically close file once done with it
+        with open(cls.CSV_FILE, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+        print("Entry added successfully")
 
 
-if __name__ == "__main__":
-    main()
+CSV.inititalize_csv()
+CSV.add_entry("20-07-2024", 125.62, "Income", "Salary")
+
+# def main():
+#     print("Hello world!")
+
+
+# if __name__ == "__main__":
+#     main()
