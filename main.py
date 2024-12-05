cd9362e3deb4899009810a59a60841f8312a68c5
Greg Hayes <gthelight@gmail.com>
2024-12-04 21:46:39 -0700

    adds get_transactions, updates gitignore

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import csv
 from datetime import datetime
-from data_entry import get_amount, get_category, get_date, get_description
+from data_entry import get_amount, get_category, get_date, get_description, date_format
 
 
 class CSV:
@@ -14,6 +14,7 @@ class CSV:
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
+            # df == DataFrame
             df = pd.DataFrame(columns=cls.COLUMNS)
             df.to_csv(cls.CSV_FILE, index=False)
 
@@ -31,6 +32,43 @@ class CSV:
             writer.writerow(new_entry)
         print("Entry added successfully")
 
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        # this is accessing all of the values of the "date" column from the CSV
+        df["date"] = pd.to_datetime(df["date"], format=date_format)
+        start_date = datetime.strptime(start_date, date_format)
+        end_date = datetime.strptime(end_date, date_format)
+
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        # returns a filtered df by locating (loc) everything where mask is True
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print('No transactinos found in the given date range')
+        else:
+            print(
+                f"Transactions from {start_date.strftime(date_format)} to {end_date.strftime(date_format)}")
+            # formatters= takes a formatting function and applies to each item of an iterable (similar to map)
+            print(filtered_df.to_string(index=False, formatters={
+                  "date": lambda x: x.strftime(date_format)}))
+
+            # looks a litle weird, but using pandas we can find every row in the category column
+            # that equals Income, then grab the amount columns from those rows and sum the values
+            total_income = filtered_df[filtered_df["category"]
+                                       == "Income"]["amount"].sum()
+
+            total_expense = filtered_df[filtered_df["category"]
+                                        == "Expense"]["amount"].sum()
+            print("\nSummary:")
+
+            # :.2f is a feature of fstrings that rounds to 2 decimal places
+            print(f"Total income: ${total_income:.2f}")
+            print(f"Total expense: ${total_expense:.2f}")
+            print(f"Net savings: ${(total_income - total_expense):.2f}")
+
+        return filtered_df
+
 
 def add():
     CSV.inititalize_csv()
@@ -42,7 +80,8 @@ def add():
     CSV.add_entry(date, amount, category, description)
 
 
-add()
+CSV.get_transactions("01-01-2023", "30-12-2024")
+# add()
 
 # CSV.inititalize_csv()
 # CSV.add_entry("20-07-2024", 125.62, "Income", "Salary")
